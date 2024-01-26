055f205ed695d5404173a5e33df9383ac5e5b686
sksjw95 <tjwldnd95@hanmail.net>
2024-01-26 10:36:01 +0900

    test2

---
+++ First/src/classpart/Alone1_1.java
@@ -0,0 +1,18 @@
+package classpart;
+
+public class Alone1_1 {
+
+	public static void main(String[] args) {
+		Alone1 man = new Alone1();
+		man.age=40;
+		man.name = "James";
+		man.isMarried=true;
+		man.babynum=3;
+		System.out.println(man.age);
+		System.out.println(man.name);
+		System.out.println(man.isMarried);
+		System.out.println(man.babynum);
+
+	}
+
+}
