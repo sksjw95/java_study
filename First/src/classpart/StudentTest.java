b1b09c0cedc5b5e32d56c44e279c24dadcb0b653
sksjw95 <tjwldnd95@hanmail.net>
2024-02-02 10:40:51 +0900

    test9

---
+++ First/src/classpart/StudentTest.java
@@ -0,0 +1,14 @@
+package classpart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student2 studentAhn = new Student2();
+		studentAhn.studentName="안승연";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+
+	}
+
+}
