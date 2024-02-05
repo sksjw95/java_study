d2f1a73ef79211d22626526014123678da101e1b
sksjw95 <tjwldnd95@hanmail.net>
2024-02-05 10:35:21 +0900

    test12

---
+++ First/src/chapter2/CharacterEx1.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class CharacterEx1 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		char ch2 = 66;
+		System.out.println(ch2);
+		
+		int ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);
+
+	}
+
+}
