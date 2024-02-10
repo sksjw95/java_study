1147f017948bfa8c9dc2fb5bed7c0ea2ba7b516a
sksjw95 <tjwldnd95@hanmail.net>
2024-02-11 00:19:26 +0900

    test18

---
+++ First/src/chapter2/Conversion2.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class Conversion2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		double b = 2.0;
+		int c = (int)(a+b); // 아마 12
+		int d = (int)(a-b); // 8???
+		int e = (int)(a*b); // 20??
+		int f = (int)(a/b); // 5??
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		
+
+	}
+
+}
