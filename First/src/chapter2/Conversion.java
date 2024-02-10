a65528e0edb5e041e907b3169e0d99354f15a66a
sksjw95 <tjwldnd95@hanmail.net>
2024-02-10 23:49:26 +0900

    test17

---
+++ First/src/chapter2/Conversion.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		int iNum = 10;
+		byte bNum = (byte) iNum;
+		System.out.println(bNum);
+		
+		int iNum2 = 1000;
+		byte bNum2 = (byte)iNum2;
+		System.out.println(bNum2);
+		
+		double dNum = 3.14;
+		int iNum3 = (int)dNum;
+		System.out.println(iNum3);
+	}
+
+}
