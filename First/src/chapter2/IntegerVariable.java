227389d7c310c14a4489da6301fbe15ced4f569e
sksjw95 <tjwldnd95@hanmail.net>
2024-02-17 13:34:32 +0900

    test24

---
+++ First/src/chapter2/IntegerVariable.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+
+
+public class IntegerVariable {
+
+	public static void main(String[] args) {
+		short sVal = 10;
+		byte bVal = 20;
+		long num=12345678900L;
+		System.out.println(sVal+bVal);
+		System.out.println(num);
+
+	}
+
+}
