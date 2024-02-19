aff9504b543bdae23533458167e574b2e91770f0
sksjw95 <tjwldnd95@hanmail.net>
2024-02-20 00:37:10 +0900

    test27

---
+++ First/src/chapter2/Variable2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int level=11;
+		String vc="dsav";
+		System.out.println(level);
+		System.out.println(vc);
+	}
+
+}
