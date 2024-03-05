00af893decb8db2517e806ca7dc3bf71ee98372b
sksjw95 <tjwldnd95@hanmail.net>
2024-03-05 09:17:41 +0900

    test41

---
+++ First/src/ifexample/AloneCodingswitchcase.java
@@ -0,0 +1,23 @@
+package ifexample;
+
+public class AloneCodingswitchcase {
+
+	public static void main(String[] args) {
+		int floor =4;
+		
+		switch(floor) {
+		case 1:System.out.println("1층 약국입니다.");
+		break;
+		case 2:System.out.println("2층 정형외과입니다.");
+		break;
+		case 3:System.out.println("3층 피부과입니다.");
+		break;
+		case 4:System.out.println("4층 치과입니다.");
+		break;
+		case 5:System.out.println("5층 헬스장입니다.");
+		break;
+		}
+
+	}
+
+}
