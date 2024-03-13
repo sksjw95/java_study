b56bd6771a0b8c849f5a35e11c14d3b0d86e172b
sksjw95 <tjwldnd95@hanmail.net>
2024-03-13 09:51:58 +0900

    test49

---
+++ First/src/loopexample/Break.java
@@ -0,0 +1,15 @@
+package loopexample;
+
+public class Break {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int num = 0;
+		for(num = 0;sum < 100;num++) {
+			sum+=num;
+		}
+		System.out.println("num : "+num);
+		System.out.println("sum : "+sum);
+	}
+
+}
