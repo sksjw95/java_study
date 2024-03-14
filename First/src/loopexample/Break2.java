18216325cfee9fbcbd7c32625eef69a61b9bd57b
sksjw95 <tjwldnd95@hanmail.net>
2024-03-14 12:56:26 +0900

    test50

---
+++ First/src/loopexample/Break2.java
@@ -0,0 +1,19 @@
+package loopexample;
+
+public class Break2 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int num;
+		for(num = 1;;num++) {
+			sum+=num;
+			if(sum>=500) {
+				break;
+			}
+		}
+		System.out.println(sum);
+		System.out.println(num);
+
+	}
+
+}
