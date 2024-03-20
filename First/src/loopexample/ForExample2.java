6bc43524540ba94b1807c2168a46b9bba3455392
sksjw95 <tjwldnd95@hanmail.net>
2024-03-21 01:28:47 +0900

    test56

---
+++ First/src/loopexample/ForExample2.java
@@ -0,0 +1,13 @@
+package loopexample;
+
+public class ForExample2 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=10; i++) {
+			//System.out.printf("안녕하세요%d\n",i);
+			System.out.println("안녕하세요"+i);
+		}
+
+	}
+
+}
